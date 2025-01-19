8cbc5964495b4656b3dd9f385cb81db5f23b0821
Ravi Kumar Singh <singhravikumar191@gmail.com>
2025-01-19 13:50:46 +0530

    login code

--- src/main/java/com/jvr/booking/repo/UserRepository.java
+++ src/main/java/com/jvr/booking/repo/UserRepository.java
@@ -1,6 +1,9 @@
 package com.jvr.booking.repo;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jvr.booking.model.User;
@@ -8,4 +11,6 @@ import com.jvr.booking.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByPhone(String phone);
+    Optional<User> findByPhone(String phone);
+
 }
--- src/main/java/com/jvr/booking/service/UserService.java
+++ src/main/java/com/jvr/booking/service/UserService.java
@@ -1,17 +1,29 @@
 package com.jvr.booking.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jvr.booking.model.User;
 import com.jvr.booking.repo.UserRepository;
+import java.util.regex.Pattern;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    
+    public UserService(UserRepository userRepository) {
+		super();
+		this.userRepository = userRepository;
+	}
+    
+ // Regex pattern for validating phone numbers
+   // private static final Pattern PHONE_NUMBER_PATTERN = Pattern.compile("^\\+?[0-9]{10,13}$");
 
+ 
     public String registerUser(User user) {
 
         // Check if phone number already exists
@@ -33,4 +45,28 @@ public class UserService {
         userRepository.save(user);
         return "User registered successfully!";
     }
+    
+	
+	public User getUserById(String phone) {
+		User user=null;
+		Optional<User> optional=userRepository.findByPhone(phone);
+		if(optional!=null)
+		{
+			user=optional.get();
+		}
+		return user;
+	}
+	public String getPhone(User user)
+	{
+		User userfetch=getUserById(user.getPhone());
+		String username=userfetch.getPhone();
+		return username;
+	}
+	public String getPassword(User user)
+	{
+		User userfetch=getUserById(user.getPhone());
+		String password=userfetch.getPassword();
+		return password;
+	} 
+
 }
